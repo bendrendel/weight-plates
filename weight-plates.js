1081adf0a9499641f82593341b209ba4cd1ddab6
bendrendel <bfdrendel@gmail.com>
2022-09-19 23:23:36 -0600

    Possible complete working version

--- weight-plates.js
+++ weight-plates.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { combinationsWithRep, round } = require('mathjs');
 
 class Plate {
@@ -5,6 +6,10 @@ class Plate {
         this.weight = weight;
         this.width = width;
         this.type = type;
+    }
+
+    get plateName() {
+        return `${this.weight}${this.type[0]}`;
     }
 
     get density() {
@@ -17,7 +22,7 @@ class PlateSet {
         this.plates = plates;
     }
 
-    get numPlates() {
+    get totalPlates() {
         return this.plates.length;
     }
 
@@ -27,6 +32,52 @@ class PlateSet {
 
     get totalWidth() {
         return round(this.plates.reduce((accumulator, current) => accumulator + current.width, 0), 2);
+    }
+
+    get minWeight() {
+        if (this.totalPlates === 0) {
+            return null;
+        } else {
+            return round(this.plates.map(plate => plate.weight).reduce((accumulator, current) => Math.min(accumulator, current)), 2);
+        }
+    }
+
+    get groupedPlates() {
+        return [...new Set(this.plates)].map(plate => {
+            return {
+                quantity: this.plates.filter(comparePlate => comparePlate == plate).length,
+                type: plate
+            }
+        })
+    }
+
+    get toString() {
+        return `[ ${this.plates.map(plate => plate.plateName).join(' + ')} = ${this.totalWeight} | ${this.totalWidth} | ${this.totalPlates}]`;
+    }
+
+    addPlate(plate, quantity = 1) {
+        for (let i = 0; i < quantity; i++) {
+            this.plates.push(plate);
+        }
+    }
+
+    merge(plateSet) {
+        plateSet.groupedPlates.forEach(groupedPlate => {
+            const index = this.groupedPlates.findIndex(compareGroupedPlate => compareGroupedPlate.type == groupedPlate.type);
+            if (index === -1) {
+                this.addPlate(groupedPlate.type, groupedPlate.quantity);
+            } else {
+                const quantityDiff = groupedPlate.quantity - this.groupedPlates[index].quantity;
+                this.addPlate(groupedPlate.type, Math.max(0, quantityDiff));
+            }
+        })
+    }
+
+    contains(plateSet) {
+        return plateSet.groupedPlates.every(groupedPlate => {
+            const index = this.groupedPlates.findIndex(compareGroupedPlate => compareGroupedPlate.type == groupedPlate.type);
+            return index === -1 || groupedPlate.quantity - this.groupedPlates[index].quantity > 0 ? false : true;
+        });
     }
 }
 
@@ -56,7 +107,7 @@ function plateCombosWithRep(plateOptions, sleeveLength, depth = 0, currentCombo 
     while (true) {
         if (currentCombo.totalWidth > sleeveLength) {
             return;
-        } else if (depth === plateOptions.numPlates) {
+        } else if (depth === plateOptions.totalPlates) {
             allCombos.push(currentCombo);
             return allCombos;
         } else {
@@ -82,6 +133,50 @@ const plateOptions = new PlateSet([
 
 const sleeveLength = 15;
 
-const testPlateCombos = plateCombosWithRep(plateOptions, 5);
+const allPlateCombos = plateCombosWithRep(plateOptions, 5);
 
-console.log(testPlateCombos.map((combo, idx) => `Combo ${idx + 1}: ${combo.numPlates} plates, ${combo.totalWeight} lbs, ${combo.totalWidth} in`))
+const allPlateCombosByWeight = [...new Set(allPlateCombos.map(plateSet => plateSet.totalWeight))].map(weight => ({ weight: weight, plateCombos: [] }));
+
+allPlateCombos.forEach(plateSet => {
+    const index = allPlateCombosByWeight.findIndex(comboWeight => comboWeight.weight === plateSet.totalWeight);
+    allPlateCombosByWeight[index].plateCombos.push(plateSet);
+});
+
+allPlateCombosByWeight.sort((a, b) => {
+    const lengthDiff = a.plateCombos.length - b.plateCombos.length;
+    return lengthDiff === 0 ? b.weight - a.weight : lengthDiff;
+});
+
+
+const solutions = [];
+
+function allSolutions(combosByWeight, solutionSet) {
+    const nextCombosByWeight = combosByWeight.filter(element => {
+        return !element.plateCombos.some(combo => solutionSet.contains(combo))
+    })
+
+    if (nextCombosByWeight.length === 0) {
+        solutions.push(solutionSet);
+        return;
+    } else {
+        nextCombosByWeight[0].plateCombos.forEach(plateCombo => {
+            const nextSolutionSet = new PlateSet([...solutionSet.plates]);
+            nextSolutionSet.merge(plateCombo);
+            allSolutions(nextCombosByWeight, nextSolutionSet);
+        })        
+    }
+}
+
+allSolutions(allPlateCombosByWeight, new PlateSet([]));
+
+console.log(solutions.length);
+solutions.sort((a, b) => a.totalPlates - b.totalPlates === 0 ? a.totalWeight - b.totalWeight : a.totalPlates - b.totalPlates).forEach(plateSet => plateSet.plates.sort((a, b) => a.weight - b.weight));
+
+const data = JSON.stringify(solutions.map(a=>a.toString), null, 4);
+
+fs.writeFile('solutions5.json', data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
